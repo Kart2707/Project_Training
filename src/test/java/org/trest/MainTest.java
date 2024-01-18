737895ffbd4157f90252d4ce5fa7242349f97ce2
Kart2707 <karthickseeni2707@gmail.com>
2024-01-19 00:10:51 +0530

    karthick's code

--- src/test/java/org/trest/MainTest.java
+++ src/test/java/org/trest/MainTest.java
@@ -1,5 +1,14 @@
 package org.trest;
 
-public class MainTest {
-
+public class MainTest 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test3");
+		System.out.println("Test4");
+		System.out.println("Test5");
+		System.out.println("Test6");
+	}
 }
